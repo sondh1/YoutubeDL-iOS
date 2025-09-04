90115bf9c576137557444ddbd43b7b3c48ab1184
sondh1 <99641560+sondh1@users.noreply.github.com>
2025-09-04 15:55:26 +0700

    add cookies to Info

--- Sources/YoutubeDL/YoutubeDL.swift
+++ Sources/YoutubeDL/YoutubeDL.swift
@@ -111,7 +111,7 @@ public struct Format: Codable {
     public var dynamic_range: String?
     public var abr: Double?
     public var vbr: Double?
-    public var tt_chain_token: String?
+    public var cookies: String?
     
     public struct DownloaderOptions: Codable {
         public var http_chunk_size: Int
